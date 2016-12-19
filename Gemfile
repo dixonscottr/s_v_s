f86e4d984a9cac337f01ca23aa694bbc6ff42813
Scott Dixon <dixon.scottr@gmail.com>
2016-12-19 16:27:35 -0500

    Set up testing

--- Gemfile
+++ Gemfile
@@ -33,6 +33,10 @@ gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'rspec-rails'
+  gem 'rails-controller-testing'
+  gem 'faker'
+  gem 'factory_girl_rails'
   gem 'byebug', platform: :mri
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   has_many :matches, foreign_key: :creator
   has_many :votes, foreign_key: :voter
   has_many :voted_for_shibas, through: :votes
@@ -6,5 +7,7 @@ class User < ApplicationRecord
   validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  has_secure_password
 
 end
