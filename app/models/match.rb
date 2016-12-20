0e4c714cc0fd80659efe9508621d2be85d1f90fb
Scott Dixon <dixon.scottr@gmail.com>
2016-12-20 10:46:44 -0500

    Add validation to ensure shibas cannot challenge themselves

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,18 +1,26 @@
 class Match < ApplicationRecord
 
-  belongs_to :creator, class_name: User
-  belongs_to :shiba_one, class_name: Shiba
-  belongs_to :shiba_two, class_name: Shiba
+  belongs_to    :creator, class_name: User
+  belongs_to    :shiba_one, class_name: Shiba
+  belongs_to    :shiba_two, class_name: Shiba
   belongs_to    :winner, class_name: Shiba
   belongs_to    :loser, class_name: Shiba
-  has_many   :votes
-  has_many   :voters, through: :votes, foreign_key: :voter_id
+  has_many      :votes
+  has_many      :voters, through: :votes, foreign_key: :voter_id
 
-  validates_presence_of :creator, :shiba_one, :shiba_two
-  validates_uniqueness_of :shiba_one, scope: :shiba_two
+  validates_presence_of   :creator, :shiba_one, :shiba_two
+  validate                :not_same_shiba
 
   def vote_count
     self.votes.count
+  end
+
+  private
+
+  def not_same_shiba
+    if shiba_one == shiba_two
+      errors.add(:shibas, "A shiba cannot challenge itself")
+    end
   end
 
 end
