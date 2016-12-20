7ce395180a749375dbe95809c087825d833e8647
Scott Dixon <dixon.scottr@gmail.com>
2016-12-20 10:46:24 -0500

    Add method to find rival shibas

--- app/models/shiba.rb
+++ app/models/shiba.rb
@@ -5,7 +5,6 @@ class Shiba < ApplicationRecord
   has_many :votes
   has_many :voters, through: :votes, class_name: User
   belongs_to :creator, class_name: User
-  # has_many :matched_shibas, class_name: Shiba
 
   validates_presence_of :name, :url
   validates_uniqueness_of :name, scope: :url
@@ -13,6 +12,13 @@ class Shiba < ApplicationRecord
 
   def matches
     Match.where("shiba_one_id = ? OR shiba_two_id = ?", self.id, self.id)
+  end
+
+  def shibas_fought
+    all_rivals = matches.map do |match|
+      match.shiba_one == self ? match.shiba_two : match.shiba_one
+    end
+    all_rivals.uniq
   end
 
 end
