788cc8d3748298de81ad8c71804e6323091483f3
Scott Dixon <dixon.scottr@gmail.com>
2017-01-03 12:12:01 -0500

    Add faker requirement

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+
 users = 10.times do
   User.create!({
     username: Faker::Internet.user_name,
